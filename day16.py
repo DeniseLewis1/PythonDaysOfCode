6282841f245e194f9b7129c21713a6764549ac4c
Denise <deniselewis1120@gmail.com>
2024-01-26 08:41:39 -0800

    Day 16

--- day16.py
+++ day16.py
@@ -1,18 +1,22 @@
 # Write a function that counts the frequency of each word in a sentence.
 import re
 
-str = input(f'Enter a sentence: ')
-str = str.lower()
-str = re.sub(r'[^\w\s]', '', str)
+def count_frequency(str):
+    str = str.lower()
+    str = re.sub(r'[^\w\s]', '', str)
 
-str_list = str.split(" ")
-str_dict = {}
+    str_list = str.split(" ")
+    str_dict = {}
 
-for word in str_list:
-    if word not in str_dict:
-        str_dict[word] = 1
-    else:
-        str_dict[word] += 1
+    for word in str_list:
+        if word not in str_dict:
+            str_dict[word] = 1
+        else:
+            str_dict[word] += 1
+    
+    return str_dict
+
+str_dict = count_frequency(input(f'Enter a sentence: '))
 
 for key, value in str_dict.items():
     print(f'{key}: {value}')

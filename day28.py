9ed7236c33bbeecd0bc6d6eb861c54919d332e4b
Denise <deniselewis1120@gmail.com>
2024-02-07 08:26:18 -0800

    Day 28

--- day28.py
+++ day28.py
@@ -4,7 +4,7 @@ try:
     str = input(f'Enter a list, separate entries with a comma: ')
     n = int(input(f'Enter the position of the element to remove: '))
     str_list = str.split(",")
-    str_list.remove(str_list[n-1])
+    str_list.pop(n-1)
     print(str_list)
 
 except Exception:
